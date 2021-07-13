d8d192754f8f5a3e23ce40422d2a3037acb12515
fedor.gannutin <fedor.gannutin@ridotto.by>
2021-07-13 06:46:03 +0300

    SLOT GAME. Part 2

---
+++ Assets/Scripts/SymbolData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Symbol Data", menuName = "Symbol Data")]
+public class SymbolData : ScriptableObject
+{
+    [SerializeField] private Sprite symbolImage;
+    [SerializeField] private float symbolCost;
+    [SerializeField] private SymbolType symbolType;
+
+    public Sprite SymbolImage => symbolImage;
+
+    public float SymbolCost => symbolCost;
+
+    internal SymbolType SymbolType => symbolType;
+}
+
