a6d4c3551eced1ce13da1bd5a259bc9520287d50
fedor.gannutin <fedor.gannutin@ridotto.by>
2021-08-02 03:38:01 +0300

    Добавлены комментарии в коде

--- Assets/Scripts/Reel.cs
+++ Assets/Scripts/Reel.cs
@@ -6,52 +6,72 @@ using UnityEngine.UI;
 public class Reel : MonoBehaviour
 {
     [SerializeField] private RectTransform[] symbolsOnReel;
-    private const float exitPosition = 140;
-    private float symbolHeigth;
-    
+    // массив иконок (10, J, A и т.д.)
     [SerializeField] private Sprite[] sprites;
-
+    // RectTransform основного холста
     [SerializeField] private RectTransform mainCanvasRT;
+
+    [SerializeField] private int reelID;
+
+    // позиция-триггер, при выходе за которую символы перемещаются наверх
+    private const float exitPosition = 140;
+    // высота символов
+    private float symbolHeigth;
+    // масштаб холста, который нужно учитывать для корректной работы в разных разрешениях экрана
     private float mainCanvasScale;
+
+    public int ReelID => reelID; // дефолтный геттер для поля reelID
 
     private void Start()
     {
-        mainCanvasScale = mainCanvasRT.lossyScale.y;
+        symbolHeigth = symbolsOnReel[0].rect.height; // получаем высоту любого из символов (т.к. все одинаковых размеров)
+
+        mainCanvasScale = mainCanvasRT.lossyScale.y; // получаем текущий масштаб холста
+                                                     // !!! не использовать в Unity методе Awake(),
+                                                     // т.к. в Awake по умолчанию присваивается Scale, установленный для
+                                                     // референсного (эталонного) разрешения, а затем в Start
+                                                     // Scale корректируется с учетом текущего разрешения экрана
     }
 
+    /** Стандартный метод Unity, который выполняется каждый кадр*/
     private void Update()
     {
-        foreach (var symbol in symbolsOnReel)
+        foreach (var symbol in symbolsOnReel) // проходим циклом по всем символам на риле
         {
-            if (symbol.position.y <= exitPosition * mainCanvasScale)
+            if (symbol.position.y <= exitPosition * mainCanvasScale) // при выходе символа за позицию-триггер:
             {
-                MoveSymbolUp(symbol);
-                ChangeSymbolSprite(symbol);
+                MoveSymbolUp(symbol); // он перемещается вверх
+                ChangeSymbolSprite(symbol); // на нем меняется спрайт (иконка)
             }
         }
     }
 
     private void ChangeSymbolSprite(RectTransform symbol)
     {
-        var random = Random.Range(0, sprites.Length);
-        symbol.GetComponent<Image>().sprite = sprites[random];
-    }
-
-    public void ResetSymbolsPosition(float reelCurrentPositionY)
-    {
-        foreach(var symbol in symbolsOnReel)
-        {
-            var symbolPos = symbol.localPosition;
-            var newPos = Mathf.Round(symbolPos.y + reelCurrentPositionY); 
-            symbol.localPosition = new Vector3(symbolPos.x, newPos);
-        }
+        var random = Random.Range(0, sprites.Length); // получаем рандомное число в диапазоне от 0 (включительно) до 10 (не включительно) 
+        symbol.GetComponent<Image>().sprite = sprites[random]; // меняем иконку символа на рандомную из массива иконок
     }
 
     private void MoveSymbolUp(RectTransform symbolRT)
     {
-        symbolHeigth = symbolRT.rect.height;
+        // рассчитываем Y позицию, в которую нужно переместить символ, чтобы он оказался сверху
         var offset = symbolRT.position.y + symbolHeigth * mainCanvasScale * symbolsOnReel.Length;
-        var newPos = new Vector3(symbolRT.position.x, offset);
+        // создаем локальную переменную типа Vector3, т.к. symbolRT.position (Transform.position) имеет тип Vector3                                                                                                                                                                                  
+        var newPos = new Vector3(symbolRT.position.x, offset); // x оставляем текущий, а y присваиваем offset
+        // перемещаем символ на новую позицию
         symbolRT.position = newPos;
     }
+
+    /** Метод ResetSymbolsPosition необходим, чтобы оставить символы в рамках маски при сбросе позиции рилов.
+     *  Принимает пройденную рилом дистанцию и отнимает ее от текущей позиции символов*/
+    public void ResetSymbolsPosition(float reelTraveledDistance)
+    {
+        foreach(var symbol in symbolsOnReel)
+        {
+            var symbolPos = symbol.localPosition; // получаем текущую позицию символа
+            var correction = Mathf.Round(symbolPos.y - reelTraveledDistance); 
+            var correctedfPos = new Vector3(symbolPos.x, correction);
+            symbol.localPosition = correctedfPos;
+        }
+    }    
 }
--- Assets/Scripts/ReelsScroll.cs
+++ Assets/Scripts/ReelsScroll.cs
@@ -8,68 +8,75 @@ public class ReelsScroll : MonoBehaviour
 {
     [SerializeField] private RectTransform[] reelsRT;
     [SerializeField] private Reel[] reels;
-    [SerializeField] private float delayStep;
-    [SerializeField] private Ease startEase;
-    [SerializeField] private Ease stopEase;
-    [SerializeField] private float boostDistance, linearDistance, stoppingDistance;
-    [SerializeField] private float boostDuration, linearDuration, stoppingDuration;
     [SerializeField] private Button playButton;
 
+    // поле для задания задержки между началом вращений рилов
+    [SerializeField] private float delayStep;
+    // кривая начала вращения   
+    [SerializeField] private Ease startEase;
+    // кривая остановки
+    [SerializeField] private Ease stopEase;
+    // позиции, в которые должны двигаться рилы при разгоне, вращении и остановке
+    [SerializeField] private float boostDistance, linearDistance, stoppingDistance; 
+    // время, за которое перемещаются рилы в эти позиции
+    [SerializeField] private float boostDuration, linearDuration, stoppingDuration;
+    // словарь для связи рилов с их RectTransform, нужный для того, чтобы не использовать "дорогой" метод GetComponent()
     private Dictionary<RectTransform, Reel> reelsDictionary;
+    // стартовая позиция рилов для возврата якорей перед началом нового вращения
     private float reelStartPositionY;
-    private float thirdReelPosX;
 
     private void Start()
     {
         reelsDictionary = new Dictionary<RectTransform, Reel>();
         for (int i = 0; i < reelsRT.Length; i++)
         {
-            reelsDictionary.Add(reelsRT[i], reels[i]);
+            reelsDictionary.Add(reelsRT[i], reels[i]); // добавление в словарь рилов Reel по ключу RectTransform 
         }
-        thirdReelPosX = reelsRT[2].position.x;
-        reelStartPositionY = reelsRT[0].localPosition.y;
+        reelStartPositionY = reelsRT[0].localPosition.y; // получем начальную позицию любого из рилов
     }
 
+    /** Метод ScrollStart срабатывает при нажатии кнопки PLAY и запускает вращения рилов */ 
     public void ScrollStart() 
     {
-        playButton.interactable = false;
+        playButton.interactable = false; // отключение интерактивности(кликабельности) кнопки PLAY
         for (int i = 0; i < reelsRT.Length; i++)
         {
             var reelRT = reelsRT[i];
-            reelRT.DOAnchorPosY(boostDistance, boostDuration)
-                .SetDelay(i * delayStep)
-                .SetEase(startEase)
-                .OnComplete(() => ScrollLinear(reelRT));
+            reelRT.DOAnchorPosY(boostDistance, boostDuration) // перемещение якоря рила в позицию boostDistance за время boostDuration
+                .SetDelay(i * delayStep) // установка задержки между вращением рилов   
+                .SetEase(startEase) // установка кривой начала вращения
+                .OnComplete(() => ScrollLinear(reelRT)); // при завершении твина выполняется метод ScrollLinear и в него передается текущий рил
         }
     }
 
     private void ScrollLinear(RectTransform reelRT)
     {
-        reelRT.DOAnchorPosY(linearDistance, linearDuration)
+        reelRT.DOAnchorPosY(linearDistance, linearDuration) // перемещение якоря рила в позицию linearDistance за время linearDuration
             .SetEase(Ease.Linear)
-            .OnComplete(() => ScrollStop(reelRT));
+            .OnComplete(() => ScrollStop(reelRT)); // при завершении твина выполняется метод ScrollLinear и в него передается текущий рил
     }
 
     private void ScrollStop(RectTransform reelRT)
     {
-        reelRT.DOAnchorPosY(stoppingDistance, stoppingDuration)
-            .SetEase(stopEase)
-            .OnComplete(() => 
+        reelRT.DOAnchorPosY(stoppingDistance, stoppingDuration) // перемещение якоря рила в позицию stoppingDistance за время stoppingDuration
+            .SetEase(stopEase) // установка кривой начала вращения
+            .OnComplete(() => // при завершении твина:
                 {
-                    PrepareReel(reelRT);
-                    if (reelRT.position.x == thirdReelPosX)
+                    PrepareReel(reelRT); // подготавливаем рилы к следующему вращению
+                    if (reelsDictionary[reelRT].ReelID == 3)
                     {
-                        playButton.interactable = true;
+                        playButton.interactable = true; // когда остановлен третий рил, кнопка PLAY становится интерактивной
                     }
                 });
     }
 
+    /** Метод PerpareReel используется для сброса якорей рилов и их символов
+     *  на начальную позицию перед началом нового вращения. */
     private void PrepareReel(RectTransform reelRT)
     {
-        var currentReelPosY = reelRT.localPosition.y;
-        print(currentReelPosY);
-        reelRT.localPosition = new Vector3(reelRT.localPosition.x, reelStartPositionY);
-        reelsDictionary[reelRT].ResetSymbolsPosition(currentReelPosY);
-
+        var prevReelPosY = reelRT.localPosition.y; // получаем текущую позицию рила, необходимую для рассчета пройденной дистанции
+        var traveledReelDistance = -(reelStartPositionY + prevReelPosY); // рассчитываем пройденную дистанцию и меняем ей знак, т.к. координаты отрицательные
+        reelRT.localPosition = new Vector3(reelRT.localPosition.x, reelStartPositionY); // сброс якорей рила 
+        reelsDictionary[reelRT].ResetSymbolsPosition(traveledReelDistance); // см. класс (скрипт) Reel
     }
 }
