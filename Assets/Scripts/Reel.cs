a951b700542e9ec3c1e17d3d4763147a8c2da376
fedor.gannutin <fedor.gannutin@ridotto.by>
2021-08-03 05:21:25 +0300

    - добавлены комментарии - немного изменена математика метода CalculateExtraDistance (возвращаемые методом значения не изменились)

--- Assets/Scripts/Reel.cs
+++ Assets/Scripts/Reel.cs
@@ -7,8 +7,6 @@ public class Reel : MonoBehaviour
 {
     [SerializeField] GameConfig gameConfig;
     [SerializeField] private RectTransform[] symbolsOnReel;
-    // массив иконок (10, J, A и т.д.)
-    [SerializeField] private Sprite[] sprites;
     // RectTransform основного холста
     [SerializeField] private RectTransform mainCanvasRT;
 
@@ -24,26 +22,26 @@ public class Reel : MonoBehaviour
     private int currentFinalSet = 0;
 
     [SerializeField] int reelId;
-    private ReelState reelState = ReelState.Stop;
-
-    public int ReelId => reelId;
+    // поле для определения текущего состояния рила и заполнения финальных экранов, работы с кнопками и т.д.
+    private ReelState reelState = ReelState.Stop; 
 
     internal ReelState ReelState { get => reelState; set => reelState = value; }
 
-    public int ReelID => reelID; // дефолтный геттер для поля reelID
+    public int ReelId => reelId; // дефолтный геттер для поля reelId
 
     private void Start()
     {
-        symbolHeigth = symbolsOnReel[0].rect.height; // получаем высоту любого из символов (т.к. все одинаковых размеров)
+        // получаем высоту любого из символов, т.к. все одинаковых размеров (можно вынести в константу или в gameConfig)
+        symbolHeigth = symbolsOnReel[0].rect.height; 
 
-        mainCanvasScale = mainCanvasRT.lossyScale.y; // получаем текущий масштаб холста
-                                                     // !!! не использовать в Unity методе Awake(),
-                                                     // т.к. в Awake по умолчанию присваивается Scale, установленный для
-                                                     // референсного (эталонного) разрешения, а затем в Start
-                                                     // Scale корректируется с учетом текущего разрешения экрана
+        // получаем текущий масштаб холста
+        // !!! не использовать в Unity методе Awake(),
+        // т.к. в Awake по умолчанию присваивается Scale, установленный для
+        // референсного (эталонного) разрешения, а затем в Start
+        // Scale корректируется с учетом текущего разрешения экрана
+        mainCanvasScale = mainCanvasRT.lossyScale.y; 
     }
-
-    /** Стандартный метод Unity, который выполняется каждый кадр*/
+    
     private void Update()
     {
         foreach (var symbol in symbolsOnReel) // проходим циклом по всем символам на риле
@@ -68,13 +66,21 @@ public class Reel : MonoBehaviour
         }
     }    
 
+    /// <summary>
+    /// Метод возвращает случайный символ из набора символов в конфиге
+    /// </summary>
+    /// <returns> Sprite randomSprite</returns>
     private Sprite GetRandomSymbol()
-    {
-        var random = Random.Range(0, gameConfig.Symbols.Length);
-        var sprite = gameConfig.Symbols[random].SymbolImage;
+    {        
+        var random = Random.Range(0, gameConfig.Symbols.Length); // получаем рандомное число в диапазоне от 0(включительно) до 10(не включительно)
+        var sprite = gameConfig.Symbols[random].SymbolImage; // меняем иконку символа на рандомную из конфига
         return sprite;
     }
 
+    /// <summary>
+    /// Метод возвращает нужный символ финального экрана из набора символов в конфиге
+    /// </summary>
+    /// <returns> Sprite finalScreenSprite</returns>
     private Sprite GetFinalScreenSymbol() 
     {
         var finalScreenSymbolIndex = currentSymbolIndex + (reelId - 1) * gameConfig.VisibleSymbolsOnReel;
@@ -88,6 +94,9 @@ public class Reel : MonoBehaviour
         return newSymbol.SymbolImage;
     }
 
+    /// <summary>
+    /// Метод перемещает символ, вышедший за границы маски вверх.
+    /// </summary>
     private void MoveSymbolUp(RectTransform symbolRT)
     {
         // рассчитываем Y позицию, в которую нужно переместить символ, чтобы он оказался сверху
